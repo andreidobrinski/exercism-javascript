2cbfd2fa889cf39467dab14dcad13d95696ddbe5
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-04-10 14:15:29 -0400

    dnd character

---
+++ dnd-character/dnd-character.js
@@ -0,0 +1,63 @@
+//
+// This is only a SKELETON file for the 'DnD Character' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const abilityModifier = (score) => {
+  if (score < 3) {
+    throw new Error('Ability scores must be at least 3');
+  }
+  if (score > 18) {
+    throw new Error('Ability scores can be at most 18');
+  }
+  return Math.floor((score - 10) / 2);
+};
+
+const rollDie = () => Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+
+export class Character {
+  constructor() {
+    this._strength = Character.rollAbility();
+    this._dexterity = Character.rollAbility();
+    this._constitution = Character.rollAbility();
+    this._intelligence = Character.rollAbility();
+    this._wisdom = Character.rollAbility();
+    this._charisma = Character.rollAbility();
+    this._hitpoints = abilityModifier(this._constitution) + 10;
+  }
+
+  static rollAbility() {
+    let rollArray = Array.from({length: 4}, () => rollDie());
+    rollArray.sort();
+    rollArray.shift();
+    return rollArray.reduce((acc, cur) => acc + cur);
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  get constitution() {
+    return this._constitution;
+  }
+
+  get intelligence() {
+    return this._intelligence;
+  }
+
+  get wisdom() {
+    return this._wisdom;
+  }
+
+  get charisma() {
+    return this._charisma;
+  }
+
+  get hitpoints() {
+    return this._hitpoints;
+  }
+}
