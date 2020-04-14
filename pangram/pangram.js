6bb7e2717e74471042cff4d625498efcc6476bcc
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-04-14 15:23:29 -0400

    pangram

---
+++ pangram/pangram.js
@@ -0,0 +1,35 @@
+const ALPHABET = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+export const isPangram = input => {
+  const inputArray = input.toLowerCase().split('');
+  const leftoverLetters = ALPHABET.filter(letter => !inputArray.includes(letter));
+
+  return !leftoverLetters.length;
+};
