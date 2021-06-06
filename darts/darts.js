4b6ae764e247bc1e97145ffa561d1c629543c382
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-06-06 14:04:57 -0400

    remove unused code

--- darts/darts.js
+++ darts/darts.js
@@ -1,10 +1,3 @@
-const DISTANCE_TO_SCORE = {
-  miss: 0,
-  outer: 1,
-  middle: 5,
-  inner: 10,
-};
-
 export const score = (x, y) => {
   const distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
   switch (true) {
